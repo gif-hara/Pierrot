901549908db8bfb6b83ad4d07ae74d2e4f78e50f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-16 23:37:45 +0900

    クレジット関連を制御出来るように

---
+++ Assets/Scripts/CreditController.cs
@@ -0,0 +1,33 @@
+﻿using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Pierrot
+{
+    /// <summary>
+    /// クレジットを制御するクラス
+    /// </summary>
+    public sealed class CreditController : MonoBehaviour
+    {
+        [SerializeField]
+        private IntReactiveProperty credit = new IntReactiveProperty(100);
+        public IReactiveProperty<int> Credit => this.credit;
+
+        private IntReactiveProperty paid = new IntReactiveProperty(0);
+        public IReactiveProperty<int> Paid => this.paid;
+
+        private IntReactiveProperty win = new IntReactiveProperty(0);
+        public IReactiveProperty<int> Win => this.win;
+
+        void Awake()
+        {
+            Broker.Global.Receive<FiredCoin>()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    _this.credit.Value--;
+                })
+                .AddTo(this);
+        }
+    }
+}
