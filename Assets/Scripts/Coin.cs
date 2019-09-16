9ddfeea508b52cbad8c18cb183fa703b5f10cb21
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-16 21:52:52 +0900

    とりあえずコインを射出出来るように

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+﻿using HK.Framework;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Pierrot
+{
+    /// <summary>
+    /// コインを制御するクラス
+    /// </summary>
+    public sealed class Coin : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D controlledRigidbody2D = null;
+        public Rigidbody2D Rigidbody2D => this.controlledRigidbody2D;
+
+        private static ObjectPoolBundle<Coin> pools = new ObjectPoolBundle<Coin>();
+
+        private ObjectPool<Coin> pool;
+
+        public Coin Rent()
+        {
+            var pool = pools.Get(this);
+            var clone = pool.Rent();
+            clone.pool = pool;
+
+            return clone;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCoin.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Pierrot
+{
+    /// <summary>
+    /// コインを生成するクラス
+    /// </summary>
+    public sealed class SpawnCoin : MonoBehaviour
+    {
+        [SerializeField]
+        private Coin coinPrefab;
+
+        [SerializeField]
+        private Transform coinParent;
+
+        [SerializeField]
+        private float angleLimit = 45.0f;
+
+        [SerializeField]
+        private float speed = 1.0f;
+
+        [SerializeField]
+        private float power = 1.0f;
+
+        private float initialAngle;
+
+        void Start()
+        {
+            this.initialAngle = this.transform.localRotation.eulerAngles.z;
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                this.Fire();
+            }
+            var angle = (Mathf.Sin(Time.time * this.speed)) * this.angleLimit;
+            this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, this.initialAngle + angle);
+        }
+
+        private void Fire()
+        {
+            var coin = this.coinPrefab.Rent();
+            coin.transform.SetParent(this.coinParent);
+            coin.transform.position = this.transform.position;
+            var vector = this.transform.up;
+            Debug.Log(vector);
+            coin.Rigidbody2D.AddForce(vector * this.power);
+        }
+    }
+}
