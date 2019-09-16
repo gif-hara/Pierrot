cecfe067999c00c16fb41c9a49a3da8c4a38dd20
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-17 01:23:50 +0900

    フレームレート設定出来るように

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Pierrot
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GameSystem : MonoBehaviour
+    {
+        [SerializeField]
+        private int targetFrameRate = 0;
+
+        void Awake()
+        {
+            Application.targetFrameRate = this.targetFrameRate;
+        }
+    }
+}
