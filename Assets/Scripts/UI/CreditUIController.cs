48e14835ec30bde3cb4ef07e228c5b75f44c94ac
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-09-17 00:16:52 +0900

    クレジットUI実装

---
+++ Assets/Scripts/UI/CreditUIController.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Pierrot
+{
+    /// <summary>
+    /// クレジットUIを制御するクラス
+    /// </summary>
+    public sealed class CreditUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshPro credit = null;
+
+        [SerializeField]
+        private TextMeshPro paid = null;
+
+        [SerializeField]
+        private TextMeshPro win = null;
+
+        [SerializeField]
+        private CreditController creditController = null;
+
+        void Awake()
+        {
+            this.creditController.Credit
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.credit.text = x.ToString();
+                })
+                .AddTo(this);
+
+            this.creditController.Paid
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.paid.text = x.ToString();
+                })
+                .AddTo(this);
+
+            this.creditController.Win
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.win.text = x.ToString();
+                })
+                .AddTo(this);
+        }
+    }
+}
